bfc053e3b40916eccab14718dd73daea6a811c2d
Silicon42 <CurtisStofer@gmail.com>
2024-04-05 11:50:59 -0700

    split boilerplate into multiple files for better organization

---
+++ inc/cl_bp_public_typedefs.h
@@ -0,0 +1,60 @@
+#ifndef CL_BP_PUBLIC_TYPEDEFS_H
+#define CL_BP_PUBLIC_TYPEDEFS_H
+/**
+ * Typedefs exposed for data-driven staged queue creation
+*/
+#include <CL/cl.h>
+//#include <stdint.h>
+#include <stdbool.h>
+
+enum rangeMode {
+//	PAD,	// pad to multiple of work group dimensions
+	EXACT,	// set range/size to param
+	SINGLE,	// meant for primarily serial workloads, [0] == false -> 1 hardware workgroup, [0] == true -> single work item
+	REL,	// expand/contract range and output relative to input
+	DIAG,	// exact on [0], contraction(- only, + no useful effect) relative to length of diagonal on [1]*, relative on [2], used for hough_lines
+	DIVIDE,	// divides each component by corresponding param
+};
+
+typedef struct {
+	enum rangeMode mode;	// what mode to calculate the NDRange/size_t[3] in
+	int param[3];			// effects execution range and size of the output buffers, see rangeMode above
+} RangeData;
+
+typedef struct {
+	uint16_t rel_ref;		// how many elements back relative to the current arg the operation is referencing
+	RangeData range;		// data on how to calculate the size_t[3] of the arg
+	bool is_host_readable;	// indicates if the host will read an output buffer, N/A for read args
+	bool is_array;			// indicates if arg should treat last dimension as array, N/A for read args
+} ArgStaging;	//TODO: since stbi only supports 8 bit depth the host readable flag forces 8 bit output which may cause calculation issues if buffer isn't last
+
+// user provided info of how to set up kernels in a queue and their arguments
+typedef struct {
+	int kernel_idx;		// index of the reference kernel provided for cloning
+	uint16_t rel_ref;	// how many elements back relative to the current end of the ArgTracker list the operation is referencing
+	RangeData range;	// data on how to calculate the NDRange
+	ArgStaging* args;	// array containing data on how to construct each arg
+} QStaging;				//TODO:^this should probably be separated out so that range isn't tied to buffer size
+
+// info used in assigning an arg to kernels, creating/reading buffers on the host, and deallocating mem objects
+typedef struct {
+	cl_mem arg;				// pointer to the image or generic cl_mem object in question
+	size_t size[3];			// used for calculating later arg sizes and host buffer sizes
+	cl_image_format format;	// used for verifying compatible channel types, spacing and read/write operations
+} TrackedArg;
+
+typedef struct {
+	TrackedArg* args;
+	uint16_t args_cnt;
+	uint16_t max_args;
+	size_t max_out_size;
+} ArgTracker;
+
+// info actually used in enqueueing kernels
+typedef struct {
+	cl_kernel kernel;	// stage's kernel pointer gets stored here
+	size_t range[3];	// stage's range size gets stored here
+	char name[32];			// name of the kernel function, only used for user convenience/debugging
+} QStage;
+
+#endif//CL_BP_PUBLIC_TYPEDEFS_H
---
+++ inc/cl_bp_utils.h
@@ -0,0 +1,42 @@
+#ifndef CL_BP_UTILS_H
+#define CL_BP_UTILS_H
+/**
+ * This file contains helper functions to cl_boilerplate.c, and is not neccessarily
+ * generally useful if you just want to use the boilerplate to create and run a 
+ * staged queue of image operations, it's provided publicly in the off chance that
+ * you do end up needing it though.
+ */
+
+//#define CL_VERSION_2_0
+//#define CL_VERSION_2_1
+#include <CL/cl.h>
+#include "cl_bp_public_typedefs.h"
+
+cl_mem createImageBuffer(cl_context context, char is_host_readable, char is_array, const cl_image_format* img_format, const size_t img_size[3]);
+// validates metadata[0 thru 2] formating and returns true if valid
+char isArgMetadataValid(char* metadata);
+cl_channel_type getTypeFromMetadata(const char* metadata, char isHostReadable);
+cl_channel_order getOrderFromMetadata(const char* metadata);
+
+// in can be NULL if mode is EXACT or SINGLE
+void calcSizeByMode(const size_t* in, const RangeData* range, size_t* out);
+
+char getDeviceRWType(cl_channel_type type);
+char getArgStorageType(cl_channel_type type);
+
+// currently assumes number of channels is the only thing important, NOT posistioning or ordering
+unsigned char getChannelCount(cl_channel_order order);
+unsigned char getChannelWidth(char metadata_type);
+
+
+// this only issues warnings to the user since they could easily have misnamed it
+// and it isn't required data unlike on writes that need new textures
+void verifyReadArgTypeMatch(cl_image_format ref_format, char* metadata);
+
+// converts an end relative arg index to a pointer to the referenced TrackedArg with error checking
+TrackedArg* getRefArg(const ArgTracker* at, uint16_t rel_ref);
+
+void setKernelArgs(cl_context context, const QStaging* stage, cl_kernel kernel, ArgTracker* at);
+
+
+#endif//CL_BP_UTILS_H
